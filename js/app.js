e0815b89e4ae7b239d1e29a87124d7b7b8b05234
Daniel <bitencourtdaniel30@gmail.com>
2025-08-26 21:08:58 -0300

    Remove todos os arquivos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 const btn = document.getElementById("btnTopo");
 
-    // Mostrar o botão quando descer 200px
-    window.onscroll = function() {
-      if (document.documentElement.scrollTop > 400) {
-        btn.style.opacity = "1";
-      } else {
-        btn.style.opacity = "0";
-      }
-    };
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 400) {
+    btn.style.opacity = "1";
+  } else {
+    btn.style.opacity = "0";
+  }
+};
 
-    // Voltar ao topo ao clicar
-    btn.onclick = function() {
-      window.scrollTo({top: 0, behavior: "smooth"});
-    };
+btn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const menu = document.getElementsByTagName("nav");
+const menuBtn = document.getElementById("menuBtn");
+function toggleMenu() {
+  const isVisible = menu[0].style.opacity === "1";
+  const main = document.querySelector("main");
+
+  menuBtn.src = isVisible ? "imagens/bars-solid-full.svg" : "imagens/xmark-solid-full.svg";
+  menu[0].style.opacity = isVisible ? "0" : "1";
+  main.style.filter = isVisible ? "none" : "blur(5px)";
+  main.style.transition = "filter 0.3s ease";
+  document.body.style.overflow = isVisible ? "auto" : "hidden";
+}
+
